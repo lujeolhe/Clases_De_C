fce9197ea7c58362af6f94b76ad9e9f5270cb6b2
luis-pabl <luispablito1terran@gmail.com>
2020-05-09 11:48:31 -0500

    Se realizo un programa que cuenta palabras y caracteres de una cadena

---
+++ cadenas_de_caracteres/contador_de_palabras.c
@@ -0,0 +1,48 @@
+/////////////////////////
+//Librerias
+/////////////////////////
+#include<stdio.h>
+/////////////////////////
+//Prototipos de Func
+/////////////////////////
+
+/////////////////////////
+// MAIN
+/////////////////////////
+int main(){
+  char  cadena3[50];
+	printf("Escribe tu frase: \n");
+
+  for(int i=0;i<=50;i++){
+    cadena3[i]=0;
+  }
+
+  gets(cadena3);
+
+  int cuenta_espacios=0;
+  for(int i=0;i<50;i++){
+    if(cadena3[i]==32)
+      cuenta_espacios++;
+  }
+  int cuenta_letras=0;
+  for(int a=0;a<50;a++){
+    if((cadena3[a]>=65&&cadena3[a]<=90) || (cadena3[a]>=97&&cadena3[a]<=122)){
+      if(cadena3[a]>=65&&cadena3[a]<=90){
+        cadena3[a]=cadena3[a]+32;
+      }
+
+      else{
+        cadena3[a]=cadena3[a]-32;
+      }
+      cuenta_letras++;
+    }
+  }
+
+
+  printf("El numero de palabras es: %d\n",cuenta_espacios+1 );
+  printf("El numero de letras total es: %d\n",cuenta_letras );
+  printf("La nueva cadeana es: %s\n",cadena3 );
+
+
+
+}
