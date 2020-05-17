e2dacdf56e6b25aaa8ebbb4457a92d39c465fac7
lujeolhe <lujeolhe@live.com.mx>
2020-05-17 13:14:59 -0500

    Se empezo a desarrallar el Juego de la vida

---
+++ Juego_de_la_Vida/juego_de_la_vida.c
@@ -0,0 +1,85 @@
+/////////////////////////
+//Librerias
+/////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/////////////////////////
+//Definiciones
+/////////////////////////
+#define __tiempo 2
+#define __renglones 10
+#define __columnas 10
+/////////////////////////
+//Funciones
+/////////////////////////
+void inicializar_matriz_3D(int a[][__renglones][__columnas]);
+void imprimir_matriz(int a[][__renglones][__columnas]);
+void dibujar_celdas(char a[__renglones][__columnas],int b[][__renglones][__columnas]);
+void imprimir_celdas(char a[__renglones][__columnas]);
+void inicializar_juego(int a[][__renglones][__columnas]);
+/////////////////////////
+//Main
+/////////////////////////
+int main(){
+  srand (time(NULL)); //semilla
+	int mapa[__tiempo][__renglones][__columnas];
+  char celdas[__renglones][__columnas];
+  inicializar_matriz_3D(mapa);
+  inicializar_juego(mapa);
+  imprimir_matriz(mapa);
+
+  dibujar_celdas(celdas,mapa);
+  imprimir_celdas(celdas);
+
+
+}
+void inicializar_matriz_3D(int a[][__renglones][__columnas]){
+
+  for(int tiempo=0;tiempo<__tiempo;tiempo++){
+    for(int renglon=0;renglon<__renglones;renglon++){
+      for(int columna=0;columna<__columnas;columna++){
+        a[tiempo][renglon][columna]=0;
+      }
+    }
+  }
+}
+void imprimir_matriz(int a[][__renglones][__columnas]){
+  for(int tiempo=0;tiempo<__tiempo;tiempo++){
+    for(int renglon=0;renglon<__renglones;renglon++){
+      for(int columna=0;columna<__columnas;columna++){
+        printf("%d ",a[tiempo][renglon][columna]);
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+}
+
+void dibujar_celdas(char a[__renglones][__columnas],int b[][__renglones][__columnas]){
+  for(int renglon=0;renglon<__renglones;renglon++){
+    for(int columna=0;columna<__columnas;columna++){
+      if(b[0][renglon][columna]==0)
+        a[renglon][columna]='*';
+      else
+        a[renglon][columna]='O';
+    }
+  }
+}
+void imprimir_celdas(char a[__renglones][__columnas]){
+  for(int renglon=0;renglon<__renglones;renglon++){
+    for(int columna=0;columna<__columnas;columna++){
+      printf("%c ",a[renglon][columna]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+void inicializar_juego(int a[][__renglones][__columnas]){
+  for(int renglon=0;renglon<__renglones;renglon++){
+    for(int columna=0;columna<__columnas;columna++){
+      a[0][renglon][columna]=rand()%2;
+    }
+  }
+}
