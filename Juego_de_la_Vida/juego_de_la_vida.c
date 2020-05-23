a8201b516f0c8cd99c58805b7e3542eba9676baa
luis-pabl <luispablito1terran@gmail.com>
2020-05-23 12:24:18 -0500

    Se realizo el programa de juego de la vida,con matrices de 3 dimensiones

--- Juego_de_la_Vida/juego_de_la_vida.c
+++ Juego_de_la_Vida/juego_de_la_vida.c
@@ -18,6 +18,10 @@ void imprimir_matriz(int a[][__renglones][__columnas]);
 void dibujar_celdas(char a[__renglones][__columnas],int b[][__renglones][__columnas]);
 void imprimir_celdas(char a[__renglones][__columnas]);
 void inicializar_juego(int a[][__renglones][__columnas]);
+void vecinos_vivos(int a[][__renglones][__columnas],int vecinos[__renglones][__columnas]);
+void imprimir_matriz_2D(int a[__renglones][__columnas]);
+void siguiente_futuro(int a[][__renglones][__columnas],int vecinos[__renglones][__columnas]);
+void actualizar_estado(int a[][__renglones][__columnas]);
 /////////////////////////
 //Main
 /////////////////////////
@@ -25,17 +29,19 @@ int main(){
   srand (time(NULL)); //semilla
 	int mapa[__tiempo][__renglones][__columnas];
   char celdas[__renglones][__columnas];
+  int vecindad[__renglones][__columnas];
   inicializar_matriz_3D(mapa);
   inicializar_juego(mapa);
-  imprimir_matriz(mapa);
-
-  dibujar_celdas(celdas,mapa);
-  imprimir_celdas(celdas);
-
-
+  for(int i=1;i<=10;i++){
+    vecinos_vivos(mapa,vecindad);
+    siguiente_futuro(mapa,vecindad);
+    dibujar_celdas(celdas,mapa);
+    imprimir_celdas(celdas);
+    actualizar_estado(mapa);
+  }
 }
-void inicializar_matriz_3D(int a[][__renglones][__columnas]){
 
+void inicializar_matriz_3D(int a[][__renglones][__columnas]){
   for(int tiempo=0;tiempo<__tiempo;tiempo++){
     for(int renglon=0;renglon<__renglones;renglon++){
       for(int columna=0;columna<__columnas;columna++){
@@ -80,6 +86,78 @@ void inicializar_juego(int a[][__renglones][__columnas]){
   for(int renglon=0;renglon<__renglones;renglon++){
     for(int columna=0;columna<__columnas;columna++){
       a[0][renglon][columna]=rand()%2;
+    }
+  }
+}
+
+void vecinos_vivos(int a[][__renglones][__columnas],int vecinos[__renglones][__columnas]){
+  for(int renglon=0;renglon<__renglones;renglon++){
+    for(int columna=0;columna<__columnas;columna++){
+      if(renglon==0&&columna==0){
+        vecinos[renglon][columna]=a[0][0][1]+a[0][1][0]+a[0][1][1];
+      }
+      else if(renglon==0&&columna==__columnas-1){
+        vecinos[renglon][columna]=a[0][0][columna-1]+a[0][1][columna]+a[0][1][columna-1];
+      }
+      else if(renglon==__renglones-1&&columna==0){
+        vecinos[renglon][columna]=a[0][renglon][1]+a[0][renglon-1][0]+a[0][renglon-1][1];
+      }
+      else if(renglon==__renglones-1&&columna==__columnas-1){
+        vecinos[renglon][columna]=a[0][renglon][columna-1]+a[0][renglon-1][columna-1]+a[0][renglon-1][columna];
+      }
+      else if(renglon==0&&(columna>=1&&columna<__columnas-1)){
+        vecinos[renglon][columna]=a[0][renglon+1][columna-1]+a[0][renglon+1][columna]+a[0][renglon+1][columna+1]+a[0][renglon][columna-1]+a[0][renglon][columna+1];
+      }
+      else if(renglon==__renglones-1&&(columna>=1&&columna<__columnas-1)){
+        vecinos[renglon][columna]=a[0][renglon-1][columna-1]+a[0][renglon-1][columna]+a[0][renglon-1][columna+1]+a[0][renglon][columna-1]+a[0][renglon][columna+1];
+      }
+      else if((renglon<__renglones-1&&renglon>=1)&&columna==0){
+        vecinos[renglon][columna]=a[0][renglon-1][columna]+a[0][renglon+1][columna]+a[0][renglon-1][columna+1]+a[0][renglon][columna+1]+a[0][renglon+1][columna+1];
+      }
+      else if((renglon<__renglones-1&&renglon>=1)&&columna==__columnas-1){
+        vecinos[renglon][columna]=a[0][renglon-1][columna]+a[0][renglon+1][columna]+a[0][renglon-1][columna-1]+a[0][renglon][columna-1]+a[0][renglon+1][columna-1];
+      }
+      else{
+        vecinos[renglon][columna]=a[0][renglon-1][columna]+a[0][renglon-1][columna-1]+a[0][renglon-1][columna+1]+a[0][renglon][columna+1]+a[0][renglon][columna-1]
+        +a[0][renglon+1][columna+1]+a[0][renglon+1][columna]+a[0][renglon+1][columna-1];
+        }
+      }
+    }
+  }
+
+  void imprimir_matriz_2D(int a[__renglones][__columnas]){
+      for(int renglon=0;renglon<__renglones;renglon++){
+        for(int columna=0;columna<__columnas;columna++){
+          printf("%d ",a[renglon][columna]);
+        }
+        printf("\n");
+      }
+      printf("\n");
+  }
+
+void siguiente_futuro(int a[][__renglones][__columnas],int vecinos[__renglones][__columnas]){
+  for(int renglon=0;renglon<__renglones;renglon++){
+    for(int columna=0;columna<__columnas;columna++){
+      if(a[0][renglon][columna]==1&&(vecinos[renglon][columna]==2||vecinos[renglon][columna]==3)){
+        a[1][renglon][columna]=1;
+      }
+      else if(a[0][renglon][columna]==1&&(vecinos[renglon][columna]<2&&vecinos[renglon][columna]>3)){
+        a[1][renglon][columna]=0;
+      }
+      else if(a[0][renglon][columna]==0&&vecinos[renglon][columna]==3){
+        a[1][renglon][columna]=1;
+      }
+      else{
+        a[1][renglon][columna]=0;
+      }
+    }
+  }
+}
+
+void actualizar_estado(int a[][__renglones][__columnas]){
+  for(int renglon=0;renglon<__renglones;renglon++){
+    for(int columna=0;columna<__columnas;columna++){
+      a[0][renglon][columna]=a[1][renglon][columna];
     }
   }
 }
