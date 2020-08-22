c7c42b770f3052729258af19d3eaac7df2539708
lujeolhe <lujeolhe@live.com.mx>
2020-08-21 22:50:05 -0500

    Se agrega dona.c

--- Samples/TextColor.c
+++ Samples/TextColor.c
@@ -18,10 +18,33 @@
 //      MAIN
 /////////////////////////
 int main(){
-  printf("Hola Mundo \x1b[33m esto es amarillo \x1b[0m Esto es negro\n" );
+  system("cls");
+  #ifdef __GNUC__
+  printf("Estamos usando GNUC\n" );
+  #endif
+
+  #ifdef __MINGW32__
+  printf("Estamos usando MINGW32\n" );
+  #endif
+
+  #ifdef __MINGW64__
+  printf("Estamos usando MINGW64\n" );
+  #endif
+
+  //system("cls");
+  printf("Bienvenido a las Torres de Hannoi\n" );
+  printf("\x1B[38;2;40;192;20m\n");//verde
+  printf("|]]]]]]\n" );
+
+  printf("\x1B[38;2;124;50;8m\n");//cafe
+  printf("|------\n" );
+
+  printf("\x1B[38;2;150;5;%dm\n",15);//rojo
+  printf("|-------\n" );
+
+  //printf("\x1B[0m\n");
+
 }
-
-
 /////////////////////////
 //Funciones
 /////////////////////////
