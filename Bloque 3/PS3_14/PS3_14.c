559a34ecf5dbfc7e128bd61531d60fcfbe14a66e
lujeolhe <lujeolhe@live.com.mx>
2020-09-06 23:25:10 -0500

    Se agega archvivo de conceptos sobre funciones

--- Bloque 3/PS3_14/PS3_14.c
+++ Bloque 3/PS3_14/PS3_14.c
@@ -23,8 +23,10 @@ int main(){
   printf("dame el valor de x para el seno(x)\n" );
   scanf("%d",&n);
   printf("Seno(%d)= ",n );
-  int aux_fac=1,cont=0,aux_posi=0,aux_nega=0,aux_fac2=1;
-  for(int i=1;i<=n;i++){
+  float aux_fac=1,cont=0,aux_posi=0,aux_nega=0,aux_fac2=1;
+  float aux_valor_anterior=0;
+  float resta=1;
+  for(int i=1;resta>=0.001;i++){
       if(i==aux_fac2){
         aux_fac*=i;
         cont++;
@@ -33,19 +35,22 @@ int main(){
         aux_fac*=i;
       }
       if(i%2!=0){
-        if(aux_fac2<=n){
+        //if(aux_fac2<=n){
           if(cont==1){
-            aux_posi=aux_posi+((pow(aux_fac2,aux_fac2))/aux_fac);
-              printf("+%d^%d/%d!",aux_fac2,aux_fac2,aux_fac2 );
+            aux_posi=aux_posi+((pow(n,aux_fac2))/aux_fac);
+              printf("+%d^%.0f/%.0f!",n,aux_fac2,aux_fac2 );
           }
           else{
-              aux_nega=aux_nega-((pow(aux_fac2,aux_fac2))/aux_fac);
+              aux_nega=aux_nega-((pow(n,aux_fac2))/aux_fac);
               cont=0;
-              printf("-%d^%d/%d!",aux_fac2,aux_fac2,aux_fac2 );
+              printf("-%d^%.0f/%.0f!",n,aux_fac2,aux_fac2 );
           }
-        }
+        //}
       aux_fac2=aux_fac2+2;
       }
+      resta=fabs(aux_valor_anterior-aux_posi+aux_nega);
+      aux_valor_anterior= aux_posi+aux_nega;
     }
-  printf("=%d",aux_posi+aux_nega );
+
+  printf("=%.5f",aux_posi+aux_nega );
 }
