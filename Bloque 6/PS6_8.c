f584d09f89037dfc4bcb4bede1e847ecc5111272
luis-pabl <luispablito1terran@gmail.com>
2020-11-19 23:18:03 -0600

    lo ultimo que hemos visto

--- Bloque 6/PS6_8.c
+++ Bloque 6/PS6_8.c
@@ -8,7 +8,7 @@
 /////////////////////////
 //Definiciones
 /////////////////////////
-#define tam_max 100
+#define tam_max 10
 /////////////////////////
 //Estructuras
 /////////////////////////
@@ -17,18 +17,18 @@
 //Declaracion de Funciones
 /////////////////////////
 void inicializar_matriz(int numeros[][tam_max],int n,int m);
-void mover_abajo(int numeros[][tam_max],int n,int m);
-void mover_arriba(int numeros[][tam_max],int n,int m,int i,int cont);
+void mover_abajo(int numeros[][tam_max],int n,int j,int m);
+void mover_arriba(int numeros[][tam_max],int n,int j,int m);
+void mover_dere(int numeros[][tam_max],int m,int n);
 void imprimir(int numeros[][tam_max],int n,int m);
 /////////////////////////
 //Funcion Principal
 /////////////////////////
 int main(){
   int numeros[tam_max][tam_max];
-  int n=4,m=5;
+  int n=10,m=10;
   inicializar_matriz(numeros,n,m);
-  numeros[0][0]=1;
-  mover_abajo(numeros,n,m);
+  mover_dere(numeros,m,n);
 }
 /////////////////////////
 //Funciones
@@ -40,43 +40,34 @@ void inicializar_matriz(int numeros[][tam_max],int n,int m){
     }
   }
 }
-void mover_abajo(int numeros[][tam_max],int n,int m){
+void mover_abajo(int numeros[][tam_max],int n,int j,int m){
   int aux=0;
-  int cont=3;
+  numeros[0][j]=1;
   imprimir(numeros,n,m);
-    for(int i=0;i<m;i++){
-      printf("Valor de i: %d\n",i );
-      for(int j=0;j<n;j++){
-        if(i%2==0){
-          numeros[j+1][i]=numeros[j][i];
-          numeros[j][i]=aux;
-          printf("Valor de j: %d\n",j );
-          if(j+1<n)
-          imprimir(numeros,n,m);
-        }
-        else{
-          mover_arriba(numeros,n,m,i,cont);
-        }
-      }
-      printf("Esto es del mover abajo\n" );
-    }
+  for(int i=0;i<n-1;i++){
+    numeros[i+1][j]=numeros[i][j];
+    numeros[i][j]=aux;
+    imprimir(numeros,n,m);
+  }
+  numeros[n-1][j]=0;
 }
-void mover_arriba(int numeros[][tam_max],int n,int m,int i,int cont){
+void mover_arriba(int numeros[][tam_max],int n, int j,int m){
   int aux=0;
-  numeros[cont][i]=1;
-  imprimir(numeros,n,m);
-  for( ;i<2;i++){
-    for(;cont>0;cont--){
-      if(i%2!=0){
-        numeros[cont-1][i]=numeros[cont][i];
-        numeros[cont][i]=aux;
-        imprimir(numeros,n,m);
-      }
-      else{
-        printf("Valor de i en mover arriba %d\n",i );
-        return;
-      }
-    }
+  int i=n;
+  numeros[i][j]=1;
+  for(;i>0;i--){
+    numeros[i-1][j]=numeros[i][j];
+    numeros[i][j]=aux;
+    imprimir(numeros,n,m);
+  }
+  numeros[0][j]=0;
+}
+void mover_dere(int numeros[][tam_max],int m,int n){
+  for(int j=0;j<m;j++){
+    if(j%2==0)
+      mover_abajo(numeros,n,j,m);
+    if(j%2!=0)
+      mover_arriba(numeros,n,j,m);
   }
 }
 void imprimir(int numeros[][tam_max],int n,int m){
